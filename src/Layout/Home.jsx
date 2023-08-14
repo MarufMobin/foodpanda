70524cc64f85129ce24e1d253ecbde06dd444962
Maruf Mobin <mobinh264@gmail.com>
2023-08-14 14:01:14 +0600

    Some folder and file er updated

---
+++ src/Layout/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../componments/home/HeroSection";
+import Navigation from "../componments/home/Navigation";
+
+const Home = () => {
+    return <div>
+        <Navigation></Navigation>
+        <HeroSection></HeroSection>
+    </div>;
+};  
+export default Home;
+  
