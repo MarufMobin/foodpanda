234bd41d6587e1c076d00d33f1e4b4f09c48ef0b
mdhasmat690 <mdhasmat690@gmail.com>
2023-08-14 13:51:49 +0600

    footer start

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../componments/home/Footer";
 
 const Main = () => {
   return (
     <div>
       <h1>test</h1>
       <Outlet />
-      <h2>test2</h2>
+
+      <Footer />
     </div>
   );
 };
---
+++ src/componments/home/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+
+function Footer() {
+  return (
+    <Box
+      sx={{
+        borderTop: "1px solid black",
+        padding: 2,
+      }}
+    >
+      This box has a 1px solid black border only on the top.
+    </Box>
+  );
+}
+
+export default Footer;
