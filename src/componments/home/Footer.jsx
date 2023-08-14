6f32289edbc077c4157c8f552bc5f59d37779d94
mdhasmat690 <mdhasmat690@gmail.com>
2023-08-14 15:16:30 +0600

    working

--- src/componments/home/Footer.jsx
+++ src/componments/home/Footer.jsx
@@ -1,3 +1,4 @@
+import { Image } from "@mui/icons-material";
 import { Box } from "@mui/material";
 
 function Footer() {
@@ -12,7 +13,15 @@ function Footer() {
         margin: "auto",
       }}
     >
-      aslkdjfa;lsdkj
+      <Box>
+        <img
+          src="https://img.freepik.com/free-photo/delicious-fried-chicken-plate_144627-27383.jpg?w=740&t=st=1692004197~exp=1692004797~hmac=1de691f9d5f0aaaaaff12578f12b7e0320b7b4f5d6e31175abc55a7fc646aaf0"
+          alt=""
+        />
+      </Box>
+      <span>
+        <Image />
+      </span>
     </Box>
   );
 }
